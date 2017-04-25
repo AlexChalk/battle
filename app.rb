5a23982c15013d34ef120b153f2cd947d3ad5a37
david gray <davidseangray@gmail.com>
2017-04-25 18:03:31 +0100

    enabling sessions

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+enable :sessions
 set :session_secret, 'super-secret'
+
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1 = params[:player_1]
-    @player2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    erb(:play)
+  end
+
+  get '/play' do
     erb(:play)
   end
 
