aebdecbd3bf314af8a1b0ace8c37d49958bac281
Alex Chalk <alexchalk17@gmail.com>
2017-04-25 13:17:52 -0400

    Implement post/get/redirect pattern

--- app.rb
+++ app.rb
@@ -12,10 +12,12 @@ set :session_secret, 'super-secret'
   post '/names' do
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
-    erb(:play)
+    redirect '/play'
   end
 
   get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:play)
   end
 
