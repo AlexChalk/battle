fb531f425c8d88286d9eb1a17c6ab3b471454a63
Alex Chalk <alexchalk17@gmail.com>
2017-04-25 11:55:18 -0400

    Pass basic feature test (page has content)

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../../app.rb'
-describe Battle do
 
-it 'tests the page has content' do
-  expect('http://localhost:4567/').to have_content("Testing infrastructure working!")
+describe "the welcome page", :type => :feature do
+
+it 'has content' do
+  visit '/'
+  expect(page).to have_content("Testing infrastructure working!")
 end
 
 end
