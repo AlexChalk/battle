cf085db85ed038ebb920ffdf59b0f419f62a8fbb
Alex Chalk <alexchalk17@gmail.com>
2017-04-26 12:05:59 -0400

    Refactor with helper method sign_in_and_play

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -1,18 +1,12 @@
 
 feature 'Testing infrastructure' do
   scenario 'can take form input and render it onscreen' do
-    visit '/'
-    fill_in('player_1', with: 'Alex')
-    fill_in('player_2', with: 'David')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Alex vs. David')
   end
 
   scenario 'can display player hp' do
-    visit '/'
-    fill_in('player_1', with: 'Alex')
-    fill_in('player_2', with: 'David')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('David	60/60hp')
   end
 
