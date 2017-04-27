bb8312a653405a0d1a1bea0db200159696ea4c2b
Alex Chalk <alexchalk17@gmail.com>
2017-04-27 07:33:54 -0400

    Implement attack to reduce hp feature

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra/base'
 require './lib/player'
 
 class Battle < Sinatra::Base
-enable :sessions
-set :session_secret, 'super-secret'
+  enable :sessions
+  set :session_secret, 'super-secret'
 
 
   get '/' do
@@ -16,13 +16,17 @@ set :session_secret, 'super-secret'
     redirect '/play'
   end
 
-  post '/confirmation' do
-    "Your attack was successful!"
+  post '/player-2-attacked' do
+    $player_2.attack
+    $message = "Your attack was successful!"
+    redirect '/play'
   end
 
   get '/play' do
     @player_1 = $player_1.name
     @player_2 = $player_2.name
+    @player_2_hp = $player_2.hp
+    @message = $message
     erb(:play)
   end
 
--- spec/features/attacking_spec.rb
+++ spec/features/attacking_spec.rb
@@ -1,13 +1,13 @@
 feature 'attacking' do
   scenario 'can attack player' do
     sign_in_and_play
-    click_button('Fire Attack')
+    click_on('Fire Attack')
     expect(page).to have_content('Your attack was successful!')
   end
 
 scenario 'reduces points of the second player' do
     sign_in_and_play
-    click_button('Fire Attack')
+    click_on('Fire Attack')
     expect(page).to have_content('David 50/60hp')
   end
 end
