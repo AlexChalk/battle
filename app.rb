c854abc3d80b41aca6d47c90429e0640d1a7572b
Alex Chalk <alexchalk17@gmail.com>
2017-04-27 09:36:44 -0400

    Reduce global variables by subsuming players into Game

--- app.rb
+++ app.rb
@@ -12,22 +12,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do 
-    $game = Game.new
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+    $game = Game.new(Player.new(params[:player_1]), Player.new(params[:player_2]))
     redirect '/play'
   end
 
   post '/player-2-attacked' do
-    $game.attack($player_2)
+    $game.attack($game.player_2)
     $message = "Your attack was successful!"
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
-    @player_2_hp = $player_2.hp
+    @player_1 = $game.player_1.name
+    @player_2 = $game.player_2.name
+    @player_2_hp = $game.player_2.hp
     @message = $message
     erb(:play)
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,21 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new}
   let(:player_1) { double :player, :receive_damage => true }
+  let(:player_2) { double :player, :receive_damage => true }
+  subject(:game) { described_class.new(player_1, player_2)}
 
-    describe '#attack' do
-      it "sends the #receive_damage message to the player" do
-        expect(player_1).to receive(:receive_damage)
-        game.attack(player_1)
-      end
+  describe '#initialize' do
+    it "requires two arguments that are saved to player instances" do
+      game = Game.new(player_1, player_2)
+      expect(game).to have_attributes(player_1: player_1, player_2: player_2)
     end
+  end
+
+  describe '#attack' do
+    it "sends the #receive_damage message to the player" do
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
+    end
+  end
 end
